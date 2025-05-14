e350f665096871ede4fd0e04abca9b1a8ef4821a
mohan laptop <konudroid@gmail.com>
2025-05-14 15:32:41 +0100

    cleanup

--- testing_functions/hello_nao.py
+++
@@ -1,8 +0,0 @@
-from naoqi import ALProxy
-
-NAO_IP = "172.18.16.27"  # Replace with your robot's IP
-NAO_PORT = 9559
-
-tts = ALProxy("ALTextToSpeech", NAO_IP, NAO_PORT)
-tts.say("Hello! I am NAO, your friendly robot.")
-
